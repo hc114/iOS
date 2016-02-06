f3f830d2cac5a2ce6cd5da56738adf2f874565ff
Ho Jeannie <hc114@duke.edu>
2016-02-06 18:57:38 -0500

    question 10. created a temp stack to store all the even values, then put them back into the now emptied original stack

--- workspace/Cracking_the_PM/src/Problem9.java
+++ workspace/Cracking_the_PM/src/Problem9.java
@@ -1,4 +1,31 @@
+import java.util.Stack;
 
+//Write a function which takes a stack as input and returns a new stack which has the elements reversed
 public class Problem9 {
+	static Stack Solution(Stack input){
+		int size = input.size();
+		Stack output = new Stack();
+		for(int i=0; i<size; i++){
+			output.push(input.pop());
+		}
+		return output;
+	}
+	public static void main(String[] args){
+		Stack input = new Stack();
+		Stack output = new Stack();
+		Stack temp = new Stack();
+		int[] a={1,2,3,4,5,6,7,8,9,10};
+		for(int i=0; i<a.length; i++){
+			input.push(a[i]);
+			temp.push(a[i]);
+		}
+		
+		output = Solution(input);
+
+		//print out the returned input
+		System.out.println(temp.toString());
+		System.out.println(output.toString());
+
+	}
 
 }
